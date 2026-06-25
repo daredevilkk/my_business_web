90a8eb6e2d9d4d2c1fcf9eca5f9fda7cb524e99a
Kishor Sinku <kishors24@iitk.ac.in>
2026-06-25 09:59:14 +0530

    Added login logout and user authentication

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,13 @@ import "./Navbar.css"
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 function Navbar() {
+  const logout = () => {
+  localStorage.removeItem("userId");
+  navigate("/login");
+};
   const navigate = useNavigate();
   const [open, setOpen] = useState(false);
+  const userId = localStorage.getItem("userId");
   return (
     <nav className="navbar">
   <div className="logo">
@@ -19,8 +24,24 @@ function Navbar() {
   <ul className={`nav-links ${open ? "show" : ""}`}>
     <li><Link to="/" onClick={() => setOpen(false)}>HOME</Link></li>
 <li><Link to="/store" onClick={() => setOpen(false)}>STORE</Link></li>
-{!localStorage.getItem("userId") && (
-  <li><Link to="/login">LOGIN</Link></li>
+{!userId ? (
+  <li>
+    <Link to="/login" onClick={() => setOpen(false)}>
+      LOGIN
+    </Link>
+  </li>
+) : (
+  <li>
+    <button
+      className="logout-btn"
+      onClick={() => {
+        logout();
+        setOpen(false);
+      }}
+    >
+      LOGOUT
+    </button>
+  </li>
 )}
 <li><Link to="/orders" onClick={() => setOpen(false)}>ORDERS</Link></li>
 <li><Link to="/help" onClick={() => setOpen(false)}>HELP</Link></li>
